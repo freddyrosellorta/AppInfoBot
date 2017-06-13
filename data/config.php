57ec4c3ca50d921e27922d4cf68d76b5b46f1cce
Mohamamd Mahdi <ebrahimi.mahdi593@gmail.com>
2017-06-13 20:19:22 +0430

    Create config.php

--- data/config.php
+++ data/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define(BOT_TOKEN, 'You Bot Token Will Be Here');
-define(ADMIN_ID, Put Admin Id Here);
+define(ADMIN_ID, 179071599); //Put Admin Id Here
 define(REDIS_HOST, '127.0.0.1');
 define(REDIS_PORT, '6379');
 define(BOT_NAME, 'AppInfo');
